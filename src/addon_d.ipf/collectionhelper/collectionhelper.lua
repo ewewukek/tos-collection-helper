497b518501fa6659864e786ecf5945044a3d7f75
ewewukek <ewewukek@gmail.com>
2018-09-16 19:47:45 +0300

    rename and localize functions

--- src/addon_d.ipf/collectionhelper/collectionhelper.lua
+++ src/addon_d.ipf/collectionhelper/collectionhelper.lua
@@ -1,25 +1,21 @@
 local acutil = require('acutil')
 
 _G['COLLECTIONHELPER'] = _G['COLLECTIONHELPER'] or {}
-local COLLECTIONHELPER = _G['COLLECTIONHELPER']
+local ch = _G['COLLECTIONHELPER']
 
-function COLLECTIONHELPER.getItemColor(itemobj)
+local function getItemColor(itemobj)
     --itemobj.ClassID
     return 'FFFF00'
 end
 
-function COLLECTIONHELPER.GET_FULL_NAME(itemobj, ...)
-
-    local color = COLLECTIONHELPER.getItemColor(itemobj)
+local function GET_FULL_NAME(itemobj, ...)
+    local color = getItemColor(itemobj)
     local name = _G['GET_FULL_NAME_OLD'](itemobj, ...)
 
     return string.format("{#%s}{ol}%s{/}{/}", color, name)
-
 end
 
 function COLLECTIONHELPER_ON_INIT(addon, frame)
-
-    acutil.setupHook(COLLECTIONHELPER.GET_FULL_NAME, "GET_FULL_NAME")
+    acutil.setupHook(GET_FULL_NAME, "GET_FULL_NAME")
     acutil.log("Collection helper loaded!")
-
 end
