96747dc8fbec41c4e854a598556880bc48c64ccc
ewewukek <ewewukek@gmail.com>
2018-09-16 19:17:19 +0300

    clean up code replaced tabs with spaces, switched to unix line endings, removed unnecessary semicolons

--- src/addon_d.ipf/collectionhelper/collectionhelper.lua
+++ src/addon_d.ipf/collectionhelper/collectionhelper.lua
@@ -1,25 +1,25 @@
-local acutil = require('acutil');
+local acutil = require('acutil')
 
-_G['COLLECTIONHELPER'] = _G['COLLECTIONHELPER'] or {};
-local COLLECTIONHELPER = _G['COLLECTIONHELPER'];
+_G['COLLECTIONHELPER'] = _G['COLLECTIONHELPER'] or {}
+local COLLECTIONHELPER = _G['COLLECTIONHELPER']
 
 function COLLECTIONHELPER.getItemColor(itemobj)
-	--itemobj.ClassID
-	return 'FFFF00';
+    --itemobj.ClassID
+    return 'FFFF00'
 end
 
 function COLLECTIONHELPER.GET_FULL_NAME(itemobj, ...)
 
-	local color = COLLECTIONHELPER.getItemColor(itemobj)
-	local name = _G['__GET_FULL_NAME'](itemobj, ...)
+    local color = COLLECTIONHELPER.getItemColor(itemobj)
+    local name = _G['__GET_FULL_NAME'](itemobj, ...)
 
-	return string.format("{#%s}{ol}%s{/}{/}", color, name);
+    return string.format("{#%s}{ol}%s{/}{/}", color, name)
 
 end
 
 function COLLECTIONHELPER_ON_INIT(addon, frame)
 
-	acutil.setupHook(COLLECTIONHELPER.GET_FULL_NAME, "GET_FULL_NAME");
-	acutil.log("Collection helper loaded!")
+    acutil.setupHook(COLLECTIONHELPER.GET_FULL_NAME, "GET_FULL_NAME")
+    acutil.log("Collection helper loaded!")
 
 end
