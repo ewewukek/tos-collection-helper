e96142ecc06d3c45bc565f9a3f470eb5e1af1c92
ewewukek <ewewukek@gmail.com>
2018-09-19 02:44:57 +0300

    extend imc_mocks\nadded several classes that mimic imcs' api

--- t/imc_mocks.lua
+++ t/imc_mocks.lua
@@ -1,6 +1,12 @@
-function reset_classes()
+require("oop")
+
+-- utility methods
+
+function imc_reset()
     class_lists = {}
     class_tables = {}
+    geCollectionTable.collections = {}
+    session = Session:new() -- imc's global table
 end
 
 function add_object (class, name, strict)
@@ -28,10 +34,13 @@ end
 
 function add_collection (name, ...)
     local collection = add_object("Collection", name)
+    local col_info = CollectionInfo:new()
     for i, item_name in ipairs({...}) do
         add_object("Item", item_name)
         collection["ItemName_"..i] = item_name
+        col_info:add(item_name)
     end
+    geCollectionTable.collections[name] = col_info
     return collection
 end
 
@@ -50,6 +59,75 @@ function add_recipe (result_name, ...)
     end
     return recipe
 end
+
+function add_to_player_collection (collection_id, item_id)
+    local pl_col = session.collections:add(collection_id)
+    pl_col:add(item_id)
+end
+
+-- imc compatible classes
+
+CollectionInfo = inherits(nil)
+
+function CollectionInfo:init ()
+    self.items = {}
+end
+
+function CollectionInfo:add (item_id)
+    table.insert(self.items, item_id)
+end
+
+function CollectionInfo:GetTotalItemCount ()
+    return #self.items
+end
+
+function CollectionInfo:GetNeedItemCount (item_id)
+    local count = 0
+    for _, id in ipairs(self.items) do
+        if id == item_id then
+            count = count + 1
+        end
+    end
+    return count
+end
+
+---
+
+PlayerCollection = inherits(CollectionInfo);
+
+PlayerCollection.GetItemCount = CollectionInfo.GetTotalItemCount
+PlayerCollection.GetItemCountByType = CollectionInfo.GetNeedItemCount
+
+---
+
+SessionCollections = inherits(nil)
+
+function SessionCollections:Get (collection_id)
+    return self["_"..collection_id]
+end
+
+function SessionCollections:add (collection_id)
+    self["_"..collection_id] = setmetatable({items = {}}, {__index = PlayerCollection})
+    return self["_"..collection_id]
+end
+
+---
+
+Session = inherits(nil)
+
+function Session:init ()
+    self.collections = SessionCollections:new()
+end
+
+function Session:GetMySession ()
+    return session
+end
+
+function Session:GetCollection ()
+    return session.collections
+end
+
+-- imc's global functions and tables
 
 function GetClassList (name)
     local list = class_lists[name]
@@ -78,3 +156,9 @@ end
 function GET_RECIPE_REQITEM_CNT (recipe, prop_name)
     return recipe[prop_name.."_count"]
 end
+
+geCollectionTable = {
+    Get = function (collection_id)
+        return geCollectionTable.collections[collection_id]
+    end,
+}
